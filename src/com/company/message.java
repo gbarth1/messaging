5e3d9557d9db9354505e3312812ee4cea8a73d0d
gbarth1 <grace.m.barth@gmail.com>
2017-01-30 14:30:31 -0500

    Initial commit

---
+++ src/com/company/message.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.*;
+public class message
+{
+    //details of message
+    // string > # of person sending the message
+    // string > the message & time received
+    String num = new String(System.in);
+    public message()
+    {
+
+    }
+
+    public void senderNum(String num)
+    {
+
+    }
+
+    public void getMessage(String mes)
+    {
+
+    }
+
+    public void timeReceived(String time)
+    {
+
+    }
+}
---
+++ src/com/company/messageList.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class messageList
+{
+    //new messages can be added
+    //stored in descending time received order
+    //The header for the add operation is public void add(Message m)
+    //messages can move from one list to another(removed from original list)
+    //no receiving list then the message not removed from original list. public void move(int position, MessageList ml)
+
+    public messageList()
+    {
+
+    }
+
+    public void senderList()
+    {
+
+    }
+
+    public void receiverList()
+    {
+
+    }
+
+
+
+}
